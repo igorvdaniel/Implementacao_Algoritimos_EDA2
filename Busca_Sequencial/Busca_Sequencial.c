b27101ab6514aa8c8ce53e9e2a77778f433c2e5a
Ígor <136092030+igorvdaniel@users.noreply.github.com>
2026-03-20 16:54:52 +0000

    Adição da busca sequencial com indexação

--- Busca_Sequencial/Busca_Sequencial.c
+++ Busca_Sequencial/Busca_Sequencial.c
@@ -4,6 +4,11 @@
 #include <string.h>
 #define EXECUCOES 1000000
 
+typedef struct indice {
+    int valor;
+    int *p;
+} Indice;
+
 int Busca_Sequencial(int valor, int *v, int tamanho)
 {
   for(int i = 0; i < tamanho; i++)
@@ -32,6 +37,24 @@ int Busca_Sequencial_c_Sentinela(int valor, int *v, int tamanho)
     return i;
 }
 
+int Busca_Sequencial_Index(int valor, Indice *v, int tamanho_vetor, int tamanho_particao)
+{
+    Indice *ultimo = &v[0];
+    for(int i = 1; (valor >= ultimo->valor && valor <= v[i].valor) && i < tamanho_vetor;)
+    {
+        if(valor >= ultimo->valor)
+        {
+            ultimo = &v[i];
+            i++;
+        }
+    }
+
+    return Busca_Sequencial(valor, ultimo->p, tamanho_particao);
+
+}
+
+
+
 
 
 
@@ -39,15 +62,23 @@ int main()
 {
     clock_t inicio, fim;
     int v[] = {
-    23, 5, 89, 12, 45, 67, 2, 91, 34, 78,
-    11, 56, 3, 80, 29, 44, 7, 62, 19, 99,
-    0, -5, 14, 37, 52, 81, 10, 22, 63, 4,
-    71, 15, 88, 31, 49, 6, 95, 27, 50, 13,
-    84, 17, 39, 66, 8, 55, 21, 74, 42
+    -5, 0, 2, 3, 4, 5, 6, 7, 8, 10,
+    11, 12, 13, 14, 15, 17, 19, 21, 22, 23,
+    27, 29, 31, 34, 37, 39, 42, 44, 45, 49,
+    50, 52, 55, 56, 62, 63, 66, 67, 71, 74,
+    78, 80, 81, 84, 88, 89, 91, 95, 99
 };
 
     int *vetor = malloc(sizeof(int)*50);
     memcpy(vetor, v, 50 * sizeof(int));
+    
+    Indice *vetor_indexacao = malloc(sizeof(Indice)*5);
+
+    for(int i = 0, j = 0; i < 50; i += 10, j++)
+    {
+        vetor_indexacao[j].valor = vetor[i];
+        vetor_indexacao[j].p = &vetor[i];
+    }
 
     inicio = clock();
 
@@ -59,6 +90,7 @@ int main()
     fim = clock();
 
     printf("Tempo busca_sequencial: %lf\n", ((double) (fim - inicio)) / CLOCKS_PER_SEC);
+
 
     
     inicio = clock();
@@ -72,6 +104,18 @@ int main()
 
     printf("Tempo busca_sequencial com sentinela: %lf\n", ((double) (fim - inicio)) / CLOCKS_PER_SEC);
 
+        inicio = clock();
+
+    for(int i = 0; i < EXECUCOES; i++)
+    {
+        Busca_Sequencial_Index(21, vetor_indexacao, 5, 10);
+    }
+
+    fim = clock();
+
+    printf("Tempo busca_sequencial com index: %lf\n", ((double) (fim - inicio)) / CLOCKS_PER_SEC);
+
     free(vetor);
+    free(vetor_indexacao);
     return 0;
 }
--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 - Este repositório tem como objetivo Implementar algoritimos de ordenação diversos, com suas variações e tentando sempre aplica-los à contextos orientados a objetos.
 
 ## Busca Sequencial
-- Foi aplicado a Busca Sequencial Normal e a com Sentinela, onde no arquivo em C podemos ver a diferença, mesmo que mínima, de ganho de desempenho entre as duas implementações. Sendo mais vísiveis com entradas na casa dos milhões.
-- Além disso foi possível aprender algo muito significativo em relação ao C#, por mais que um código possa parecer mais simples, ele pode não ser o mais otimizado, neste caso a lição específica é que retornar NULL é mais rápido que retornar um Objeto. 
+- Foi aplicado a Busca Sequencial Normal, com Sentinela e com Indexação onde no arquivo em C podemos ver a diferença, mesmo que mínima, de ganho de desempenho entre as três implementações. Sendo mais vísiveis com entradas na casa dos milhões. Em programas de larga escala com toda certeza essa diferença é significativa.
+
+- Além disso foi possível aprender algo muito significativo em relação ao C#, por mais que um código possa parecer mais simples, ele pode não ser o mais otimizado, neste caso a lição específica é que retornar NULL é mais rápido que retornar um Objeto.
